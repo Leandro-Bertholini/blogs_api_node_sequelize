1cf152deed1780776e4247aaf769af2ec62ee645
Leandro Bertholini <bertholinileandro1@gmail.com>
2023-04-10 17:53:31 -0300

    feat: requirement_7

--- src/models/Category.js
+++ src/models/Category.js
@@ -6,6 +6,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Category = sequelize.define('Category', {
 
+    id: DataTypes.INTEGER,
     name: DataTypes.STRING
   }, {
     tableName: 'categories',
