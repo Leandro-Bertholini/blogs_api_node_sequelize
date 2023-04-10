1643aae0c976a9feab13010647faadbbeec61744
Leandro Bertholini <bertholinileandro1@gmail.com>
2023-04-09 22:56:13 -0300

    feat: Criação da migration categories

---
+++ src/migrations/20230410014148-create-categories.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('categories', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      }
+    })
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('categories');
+  }
+};
