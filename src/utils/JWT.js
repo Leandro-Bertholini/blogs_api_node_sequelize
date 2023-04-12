3ce86fbe281fd9fc435f3c7cc79c7873a4646199
Leandro Bertholini <bertholinileandro1@gmail.com>
2023-04-11 22:26:23 -0300

    feat: Construção da função para autenticar o token

--- src/utils/JWT.js
+++ src/utils/JWT.js
@@ -2,21 +2,35 @@ const jwt = require('jsonwebtoken');
 
 const TOKEN_SECRET = process.env.JWT_SECRET || 'mySecret';
 
-// Objeto de configuração adicional
+// Objeto de configuração adicional => config do JWT
 
-// const jwtConfig = {
-//   expiresIn: '15d', // expiração determinada
-//   algorithm: 'HS256' // Algoritmo utilizado para encodar
-// }
+const jwtConfig = {
+  expiresIn: '15d',
+  algorithm: 'HS256' // Algoritmo utilizado para encodar
+}
 
-const generateToken = (payload) => jwt.sign(payload, TOKEN_SECRET);
+const generateToken = (payload) => jwt.sign(payload, TOKEN_SECRET, jwtConfig);
 
-// const authenticateToken = async (req, res) => {
-//   const token = req.headers.authorization;
+const authenticateToken = async (token) => {
+  // const TOKEN = req.headers.authorization;
 
-//   const
-// };
+  if (!token) {
+    return res.status(401).json({ message: 'Token not found'});
+  };
+
+  try{
+    const decodedToken = await jwt.verify(token, TOKEN_SECRET);
+    return decodedToken;
+  } catch(err) {
+
+    const error = new Error(error.status(401)
+      .json({ message: 'Expired or invalid token' }));
+
+    return error;
+  }
+};
 
 module.exports = {
   generateToken,
+  authenticateToken
 };
